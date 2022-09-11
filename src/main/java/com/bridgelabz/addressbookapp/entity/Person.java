45e8b6a3a82907382e3961f27a5415dec90848a4
Mohd Fardeen Khan <96385727+mohdfardeenkhan@users.noreply.github.com>
2022-09-11 03:30:22 -0700

    using lombok library

--- src/main/java/com/bridgelabz/addressbookapp/entity/Person.java
+++ src/main/java/com/bridgelabz/addressbookapp/entity/Person.java
@@ -1,7 +1,11 @@
 package com.bridgelabz.addressbookapp.entity;
 
 import com.bridgelabz.addressbookapp.dto.AddressBookDto;
+import lombok.Getter;
+import lombok.Setter;
 
+@Getter
+@Setter
 public class Person {
     private int personId;
     private String firstName;
@@ -25,85 +29,5 @@ public class Person {
         this.zipCode = addressBookDto.zipCode;
         this.phoneNumber = addressBookDto.phoneNumber;
         this.emailId = addressBookDto.emailId;
-    }
-
-    public int getPersonId() {
-        return personId;
-    }
-
-    public void setPersonId(int personId) {
-        this.personId = personId;
-    }
-
-    public String getFirstName() {
-        return firstName;
-    }
-
-    public void setFirstName(String firstName) {
-        this.firstName = firstName;
-    }
-
-    public String getLastName() {
-        return lastName;
-    }
-
-    public void setLastName(String lastName) {
-        this.lastName = lastName;
-    }
-
-    public String getGender() {
-        return gender;
-    }
-
-    public void setGender(String gender) {
-        this.gender = gender;
-    }
-
-    public String getAddress() {
-        return address;
-    }
-
-    public void setAddress(String address) {
-        this.address = address;
-    }
-
-    public String getCity() {
-        return city;
-    }
-
-    public void setCity(String city) {
-        this.city = city;
-    }
-
-    public String getState() {
-        return state;
-    }
-
-    public void setState(String state) {
-        this.state = state;
-    }
-
-    public int getZipCode() {
-        return zipCode;
-    }
-
-    public void setZipCode(int zipCode) {
-        this.zipCode = zipCode;
-    }
-
-    public Long getPhoneNumber() {
-        return phoneNumber;
-    }
-
-    public void setPhoneNumber(Long phoneNumber) {
-        this.phoneNumber = phoneNumber;
-    }
-
-    public String getEmailId() {
-        return emailId;
-    }
-
-    public void setEmailId(String emailId) {
-        this.emailId = emailId;
     }
 }
