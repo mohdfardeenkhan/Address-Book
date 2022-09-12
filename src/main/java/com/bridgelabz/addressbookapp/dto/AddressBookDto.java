f339bac57cdefdcb6ef3520463a06c07601f5e1a
Mohd Fardeen Khan <96385727+mohdfardeenkhan@users.noreply.github.com>
2022-09-11 22:46:05 -0700

    Validation to name field

--- src/main/java/com/bridgelabz/addressbookapp/dto/AddressBookDto.java
+++ src/main/java/com/bridgelabz/addressbookapp/dto/AddressBookDto.java
@@ -3,10 +3,15 @@ package com.bridgelabz.addressbookapp.dto;
 import lombok.AllArgsConstructor;
 import lombok.ToString;
 
+import javax.validation.constraints.*;
+
+
 @ToString
 @AllArgsConstructor
 public class AddressBookDto {
+    @Pattern(regexp = "^[A-Z]{1}[a-zA-Z\\s]{2,}$", message = "First Name is Invalid !")
     public String firstName;
+    @Pattern(regexp = "^[A-Z]{1}[a-zA-Z\\s]{2,}$", message = "Last Name is Invalid !")
     public String lastName;
     public String gender;
     public String address;
