48a7d968994fc3b23c8196d5f18306bb5f7ade0f
Mohd Fardeen Khan <96385727+mohdfardeenkhan@users.noreply.github.com>
2022-09-12 00:09:45 -0700

    JWT & JMS

--- src/main/java/com/bridgelabz/addressbookapp/repository/AddressBookRepository.java
+++ src/main/java/com/bridgelabz/addressbookapp/repository/AddressBookRepository.java
@@ -3,7 +3,19 @@ package com.bridgelabz.addressbookapplication.repository;
 import com.bridgelabz.addressbookapplication.entity.AddressBookData;
 import org.springframework.data.jpa.repository.JpaRepository;
 
-public interface AddressBookRepository extends JpaRepository<AddressBookData, Integer> {
+import java.util.List;
+import java.util.Optional;
+
+public interface AddressBookRepository extends JpaRepository<AddressBookData, Long> {
+
+    List<AddressBookData> findByFirstName(String firstName);
+    List<AddressBookData> findByLastName(String lastName);
+    List<AddressBookData> findByGender(String gender);
+    List<AddressBookData> findByAddress(String address);
+    List<AddressBookData> findByCity(String city);
+    List<AddressBookData> findByState(String state);
+    List<AddressBookData> findByZipCode(String zipCode);
+    List<AddressBookData> findByPhoneNumber(String phoneNumber);
+    Optional<AddressBookData> findByEmailId(String emailId);
 
 }
-
