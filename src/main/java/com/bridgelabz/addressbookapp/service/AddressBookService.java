1c62b4bed31c28b7093a61d82dbd8a3a28b9ce96
Mohd Fardeen Khan <96385727+mohdfardeenkhan@users.noreply.github.com>
2022-09-12 00:12:03 -0700

    JWT & JMS

--- src/main/java/com/bridgelabz/addressbookapp/service/AddressBookService.java
+++ src/main/java/com/bridgelabz/addressbookapp/service/AddressBookService.java
@@ -1,15 +1,27 @@
-package com.bridgelabz.addressbookapp.service;
+package com.bridgelabz.addressbookapplication.service;
 
-import com.bridgelabz.addressbookapp.dto.AddressBookDto;
-import com.bridgelabz.addressbookapp.entity.Person;
+import com.bridgelabz.addressbookapplication.dto.AddressBookDTO;
+import com.bridgelabz.addressbookapplication.dto.ResponseDTO;
+import com.bridgelabz.addressbookapplication.entity.AddressBookData;
 
 import java.util.List;
+import java.util.Optional;
 
 public interface AddressBookService {
-    List<Person> getAddressBookDetails();
-    Person getAddressDetailsById(int personId);
-    Person createAdddressBookDetails(AddressBookDto addressBookDto);
-    Person updateAddressBookDetails(int personId, AddressBookDto addressBookDto);
-    void deleteAddressDetails(int personId);
+    List<AddressBookData> getAddressBookData(String token);
+    AddressBookData getAddressBookDataById(long personId);
+    List<AddressBookData> getPersonByFirstName(String firstName);
+    List<AddressBookData> getPersonByLastName(String lastName);
+    List<AddressBookData> getPersonByGender(String gender);
+    List<AddressBookData> getPersonByAddress(String address);
+    List<AddressBookData> getPersonByCity(String city);
+    List<AddressBookData> getPersonByState(String state);
+    List<AddressBookData> getPersonByZipCode(String zipCode);
+    List<AddressBookData> getPersonByPhoneNumber(String phoneNumber);
+    Optional<AddressBookData> getPersonByEmailId(String emailId);
+    AddressBookData createAddressBookData(AddressBookDTO addressBookDTO);
+    AddressBookData updateAddressBookData(int personId, AddressBookDTO addressBookDTO);
+    void deleteAddressBookData(int personId);
 
+    ResponseDTO login(String emailId, String password);
 }
