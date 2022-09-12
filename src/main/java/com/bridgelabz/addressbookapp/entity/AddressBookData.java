af4b1c5dcdd50943d09386a55159dfff233066f4
Mohd Fardeen Khan <96385727+mohdfardeenkhan@users.noreply.github.com>
2022-09-12 00:05:01 -0700

    JWT & JMS

---
+++ src/main/java/com/bridgelabz/addressbookapp/entity/AddressBookData.java
@@ -0,0 +1,148 @@
+package com.bridgelabz.addressbookapplication.entity;
+
+import com.bridgelabz.addressbookapplication.dto.AddressBookDTO;
+import lombok.Data;
+
+import javax.persistence.*;
+
+@Entity
+@Table(name = "user_address_book")
+public @Data class AddressBookData {
+
+    @Id
+    @GeneratedValue(strategy = GenerationType.AUTO)
+    @Column(name = "personId")
+    private Long personId;
+    @Column(name = "first_name")
+    private String firstName;
+    @Column(name = "last_name")
+    private String lastName;
+    @Column(name = "gender")
+    private String gender;
+    @Column(name = "address")
+    private String address;
+    @Column(name = "city")
+    private String city;
+    @Column(name = "state")
+    private String state;
+    @Column(name = "zip_code")
+    private String zipCode;
+    @Column(name = "phone_number")
+    private String phoneNumber;
+
+
+    @Column(name = "emailId")
+    private String emailId ;
+
+    private String password;
+
+    public AddressBookData(){
+
+    }
+
+    public AddressBookData(AddressBookDTO addressBookDTO){
+        this.updateAddressBookData(addressBookDTO);
+    }
+    public void updateAddressBookData(AddressBookDTO addressBookDTO){
+        this.firstName = addressBookDTO.firstName;
+        this.lastName = addressBookDTO.lastName;
+        this.gender = addressBookDTO.gender;
+        this.address = addressBookDTO.address;
+        this.city = addressBookDTO.city;
+        this.state = addressBookDTO.state;
+        this.zipCode = addressBookDTO.zipCode;
+        this.phoneNumber = addressBookDTO.phoneNumber;
+        this.emailId = addressBookDTO.emailId;
+        this.password = addressBookDTO.password;
+    }
+
+    public Long getPersonId() {
+        return personId;
+    }
+
+    public void setPersonId(Long personId) {
+        this.personId = personId;
+    }
+
+    public String getFirstName() {
+        return firstName;
+    }
+
+    public void setFirstName(String firstName) {
+        this.firstName = firstName;
+    }
+
+    public String getLastName() {
+        return lastName;
+    }
+
+    public void setLastName(String lastName) {
+        this.lastName = lastName;
+    }
+
+    public String getGender() {
+        return gender;
+    }
+
+    public void setGender(String gender) {
+        this.gender = gender;
+    }
+
+    public String getAddress() {
+        return address;
+    }
+
+    public void setAddress(String address) {
+        this.address = address;
+    }
+
+    public String getCity() {
+        return city;
+    }
+
+    public void setCity(String city) {
+        this.city = city;
+    }
+
+    public String getState() {
+        return state;
+    }
+
+    public void setState(String state) {
+        this.state = state;
+    }
+
+    public String getZipCode() {
+        return zipCode;
+    }
+
+    public void setZipCode(String zipCode) {
+        this.zipCode = zipCode;
+    }
+
+    public String getPhoneNumber() {
+        return phoneNumber;
+    }
+
+    public void setPhoneNumber(String phoneNumber) {
+        this.phoneNumber = phoneNumber;
+    }
+
+    public String getEmailId() {
+        return emailId;
+    }
+
+    public void setEmailId(String emailId) {
+        this.emailId = emailId;
+    }
+
+    public String getPassword() {
+        return password;
+    }
+
+    public void setPassword(String password) {
+        this.password = password;
+    }
+
+}
+
