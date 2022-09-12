d074afb0ec23d8ca99d467703beacd98d3d65087
Mohd Fardeen Khan <96385727+mohdfardeenkhan@users.noreply.github.com>
2022-09-12 00:15:13 -0700

    JWT & JMS

--- src/main/java/com/bridgelabz/addressbookapp/AddressBookAppApplication.java
+++ src/main/java/com/bridgelabz/addressbookapp/AddressBookAppApplication.java
@@ -1,16 +1,15 @@
-package com.bridgelabz.addressbookapp;
+package com.bridgelabz.addressbookapplication;
 
-import lombok.extern.slf4j.Slf4j;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
-import org.springframework.context.ApplicationContext;
+import springfox.documentation.swagger2.annotations.EnableSwagger2;
 
 @SpringBootApplication
-@Slf4j
-public class AddressBookAppApplication {
+
+public class AddressBookApplication {
+
     public static void main(String[] args) {
-        ApplicationContext context = SpringApplication.run(AddressBookAppApplication.class, args);
-        log.info("Address Book Strated in {} Environment", context.getEnvironment().getProperty("environment"));
-        log.info("Address Book DB User in {}", context.getEnvironment().getProperty("spring.datasource.username"));
+        SpringApplication.run(AddressBookApplication.class, args);
     }
+
 }
